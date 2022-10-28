4699337c1d27e681b2a9d2b2dcb5e203c41a47af
dimasptrautmaa <dimasptraautmaa@gmail.com>
2022-10-29 05:41:56 +0700

    update portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,8 @@
 const img = document.getElementById('img-person');
 const menu = document.getElementById('menu');
 const navul = document.getElementById('nav-menu');
+const textperson = document.getElementById('text-person');
+const img2= document.getElementById('img-dimas-2');
 
 menu.addEventListener('click', function(){
     navul.classList.toggle('move');
@@ -9,5 +11,7 @@ menu.addEventListener('click', function(){
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
 
-    img.style.translate = value * -0.5 +'px';
+    img.style.translate = value * -0.45 +'px';
+    textperson.style.translate = value * 0.25 + 'px';
+    img2.style.translate = value * 0.75 + 'px';
 });
