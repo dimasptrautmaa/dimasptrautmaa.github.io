f6296fda67d8de5f9907231f70b7c9bc6e836d9b
dimasptrautmaa <dimasptraautmaa@gmail.com>
2022-10-31 18:23:32 +0700

    bug fix

--- javascript/script.js
+++ javascript/script.js
@@ -4,15 +4,14 @@ const navul = document.getElementById('nav-menu');
 const textperson = document.getElementById('text-person');
 const img2= document.getElementById('img-dimas-2');
 
-menu.addEventListener('click', function(){
-    navul.classList.toggle('move');
-});
-
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-
+    
     img.style.translate = value * -0.45 +'px';
     textperson.style.translate = value * 0.25 + 'px';
-    img2.style.translate = value * 0.75 + 'px';
     navul.classList.remove('move');
+});
+
+menu.addEventListener('click', function(){
+    navul.classList.toggle('move');
 });
