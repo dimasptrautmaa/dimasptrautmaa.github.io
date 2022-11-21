e9d7f9a2dd9009cc843c7d572c96ac763d08280b
dimasptrautmaa <dimasptraautmaa@gmail.com>
2022-11-21 23:53:48 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,6 @@ const navul = document.getElementById('nav-menu');
 const textperson = document.getElementById('text-person');
 const img2= document.getElementById('img-dimas-2');
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-
-    img.style.translate = value * -0.45 +'px';
-    textperson.style.translate = value * 0.25 + 'px';
-});
-
 menu.addEventListener('click', function(){
     navul.classList.toggle('move');
 });
